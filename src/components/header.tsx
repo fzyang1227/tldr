2d1984acad1f8d48d9bd9b924674b114eed5c5f4
Felix Yang <fzyang1227@gmail.com>
2023-02-11 21:55:36 -0500

    almost done with header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,18 @@ const TopHeader = () =>
 <header className="p-3 text-bg-dark sticky-top">
     <div className="container">
         <div className="d-flex justify-content-between align-items-center justify-content-center">
-            <div className="row">
-                <img src="./logo192.png" // insert logo here
-                alt="tl;dr logo"
-                className="align-middle col-sm"></img>
-                <h3 className="align-middle col-sm">tl;dr</h3>
+            <div className="d-flex justify-content-between align-items-center justify-content-center">
+                <img src="../assets/gear-32.png" // insert logo here
+                    alt="tl;dr logo"
+                    className="ratio-1x1"></img>
+                <div>
+                    <h3>tl;dr</h3>
+                </div>
             </div>
             <div className='text-end align-middle'>
-                
+                <img src="../assets/gear-32.png" // insert logo here
+                    alt="options"
+                    className="ratio-1x1"></img>
             </div>
         </div>
     </div>
