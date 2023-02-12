9c23f2b5caa37faca0a576e547a12c54c0d2f022
Felix Yang <fzyang1227@gmail.com>
2023-02-11 21:41:31 -0500

    basic headings and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import App2 from './components/app';
+import TopHeader from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopHeader/ >
+      <App2/ >
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+const TopHeader = () =>
+<header className="p-3 text-bg-dark sticky-top">
+    <div className="container">
+        <div className="d-flex justify-content-between align-items-center justify-content-center">
+            <div className="row">
+                <img src="./logo192.png" // insert logo here
+                alt="tl;dr logo"
+                className="align-middle col-sm"></img>
+                <h3 className="align-middle col-sm">tl;dr</h3>
+            </div>
+            <div className='text-end align-middle'>
+                
+            </div>
+        </div>
+    </div>
+  </header>
+export default TopHeader
