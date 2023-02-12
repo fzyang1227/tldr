3f9aa3118077c9b6629566bea71b5fb4ed1c67de
Julie Wang <juliewang@Julies-MacBook-Pro.local>
2023-02-12 01:33:23 -0500

    lets fricking go

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState }from "react";
 import "./App.css"; 
+import messagesFromReactAppListener from "./chromeServices/DOMEvaluator";
 
 function App() {
 
   const [currUrl, setCurrUrl] = useState("");
+  const [summary, setSummary] = useState("");
 
   async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
@@ -16,11 +18,20 @@ function App() {
     setCurrUrl(tab.url as string)
   });
 
+  // chrome.runtime.sendMessage(
+  //   {greeting: 'helo'}
+  // );
+
+  messagesFromReactAppListener(currUrl).then((response: any) => {
+    console.log('ah');
+    console.log(response);
+    setSummary(response.summary.data.sm_api_content)
+  });
 
   return (
     <div className="App">
       <header className="App-header">
-        {currUrl}
+        {summary}
       </header>
     </div>
   );
--- src/chromeServices/DOMEvaluator.ts
+++ src/chromeServices/DOMEvaluator.ts
@@ -1,30 +1,65 @@
 import { DOMMessage, DOMMessageResponse } from "../types";
 // @ts-ignore
-import * as smmry from "smmry";
+import * as smmry from "smmry"; 
+//@ts-ignore
+import * as rp from "require-promise";
+//@ts-ignore
+import axios from 'axios';
+//@ts-ignore
+import * as assignIn from "lodash.assignin";
 
 let summary = "";
 
-// Function called when a new message is received
-const messagesFromReactAppListener = (
-  msg: DOMMessage,
-  sender: chrome.runtime.MessageSender,
-  sendResponse: (response: DOMMessageResponse) => void
-) => {
-  console.log("[content.js]. Message received", msg);
+const API_URL = 'https://api.smmry.com';
 
-  smmry.summarizeURL(sender.url, {
-    SM_LENGTH: 5,
+
+const buildQuery = (query: any) => Object.keys(query)
+        .reduce((acc, val) => {
+          if (!query[val]) return acc;
+          return (`${acc}${((acc && '&') || '?')}${val}=${query[val]}`);
+        }, '');
+
+const defaultOptions = {
+  SM_API_KEY: '2CD97EA006',
+  SM_LENGTH: undefined,
+  SM_KEYWORD_COUNT: undefined,
+  SM_WITH_BREAK: undefined,
+  SM_WITH_ENCODE: undefined,
+  SM_IGNORE_LENGTH: undefined,
+  SM_QUOTE_AVOID: undefined,
+  SM_QUESTION_AVOID: undefined,
+  SM_EXCLAMATION_AVOID: undefined,
+};
+
+// Function called when a new message is received
+const messagesFromReactAppListener = (url: string
+) => {
+  const opts = assignIn(defaultOptions); //options is another param i took out
+
+  const query = buildQuery(assignIn(opts, {
+    SM_LENGTH: 4,
+  }, { SM_URL: encodeURIComponent(url) }));
+  const rpOpts = {
+    method: 'GET',
+    url: `${API_URL}${query}`,
+  };
+  return axios.request(rpOpts).then((data: any) => {
+    summary = data;
+    const response: DOMMessageResponse = {
+      summary: summary,
+    }
+
+    // console.log(response);
+
+
+    return response;
   });
 
-  // Prepare the response object with information about the site
-  const response: DOMMessageResponse = {
-    summary: "",
-  };
-
-  sendResponse(response);
 };
 
 /**
  * Fired when a message is sent from either an extension process or a content script.
  */
-chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
+// chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
+
+export default messagesFromReactAppListener;
